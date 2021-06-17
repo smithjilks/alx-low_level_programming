3972653a8cace5be29332c7f551c8a9731c35af7
smithjilks <smithjilks@gmail.com>
2021-06-17 14:38:04 +0300

    Update 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float myFloat;
 printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
 printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
 printf("Size of a long int: %lu byte(s)\n", sizeof(myLong));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(myChar));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(myLongLongInt));
 printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
 return (0);
 }
