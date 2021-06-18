bb247dc4787a0c3021e7fb3b61b55921902910b6
smithjilks <smithjilks@gmail.com>
2021-06-18 20:41:02 +0300

    Add 5-print_numbers.c script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*file: 5-print_numbers.c*/
+/**
+ * main - prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
