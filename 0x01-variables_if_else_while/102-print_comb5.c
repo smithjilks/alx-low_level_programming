0159b2ad613defcb07307fba2e70b076a8d429b5
smithjilks <smithjilks@gmail.com>
2021-06-18 22:00:37 +0300

    Add 102-print_comb5.c file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/** file: 102-print_comb5.c*/
+/**
+ * main - prints all possible different combinations of three digits.
+ *
+ * Return: Alwabs 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+    a = 48; 
+    b = 48;
+    c = 48;
+    d = 49;
+
+	while (a < 58)
+	{
+		while (b < 58)
+		{
+			while (c < 58)
+			{
+				while (d < 58)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (!(a == 57 &&
+						b == 56 &&
+						c == 57 &&
+						d == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					d++;
+				}
+				c++;
+				d = 48;
+			}
+			b++;
+			c = a;
+			d = b + 1;
+		}
+		a++;
+		b = 48;
+		c = a;
+		d = b + 1;
+	}
+	putchar('\n');
+	return (0);
+}
