a10b622f79aa54fc67b8b851c73525b47ab0dd7e
smithjilks <smithjilks@gmail.com>
2021-06-22 17:58:25 +0300

    Add 0-holbertom.c file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/*file: 0-holberton.c*/
+/**
+ * main - prints Holberton, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Holberton";
+	int i, n;
+
+	n = sizeof(str) / sizeof(str[0]);
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,5 @@
 # 0x02-functions_nested_loops
+
+0-holberton.c prints Holberton, followed by a new line.
+
+        - The program should return 0
