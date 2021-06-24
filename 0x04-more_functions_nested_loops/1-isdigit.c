a2f20d5a75a41ab170a6f355a144494e835f1d50
smithjilks <smithjilks@gmail.com>
2021-06-24 11:24:11 +0300

    Add 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/*file: 1-isdigit.c*/
+/**
+ * _isdigit - checks for a digit 0 through 9
+ *
+ * @c: parameter for the _isdigit function
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	return (0);
+}
