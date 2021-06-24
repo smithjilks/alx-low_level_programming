9be1af1ceb6a10e98155d615f12fc5864725b7be
smithjilks <smithjilks@gmail.com>
2021-06-24 11:12:28 +0300

    Add 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberon.h"
+/* file: 0-isupper.c */
+/**
+ * _isupper- checks for uppercase charcter
+ *
+ * @c: parameter for the _isupper function.
+ *
+ * Return: 1 if upper, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
