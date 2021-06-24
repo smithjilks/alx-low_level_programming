d4cff239504771e616b723f6b83ef0ba26216be9
smithjilks <smithjilks@gmail.com>
2021-06-24 17:28:26 +0300

    Add 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - tests function that prints if integer is positive or negtive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_ot_negative(i);
+
+	return (0);
+}
