da60db8842bc6bba4662f25be5b6022305950d72
smithjilks <smithjilks@gmail.com>
2021-06-30 15:10:08 +0300

    Add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/*file: 1-strncat.c*/
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string destination pointer parameter
+ * @src: string source pointer parameter
+ * @n: number of bytes from source
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) == *(src + j);
+
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+}
