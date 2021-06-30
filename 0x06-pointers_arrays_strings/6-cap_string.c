97f79eb290ff0245ca47276ce8ff3fbc5048b415
smithjilks <smithjilks@gmail.com>
2021-06-30 17:25:36 +0300

    Add 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/*file: 6-cap_string.c*/
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str: string pointer paramter
+ *
+ * Return: pointer to the modifed string str
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 97 && *(str + i) <= 122
+		    && *(str + (i - 1)) == 32 && i != 0)
+			*(str + i) = 65 + (*(str + i) - 97);
+
+		if ((*(str + i) >= 97 && *(str + i) <= 122
+		    && i == 0) || (*(str + (i - 1)) == 10))
+			*(str + i) = 65 + (*(str + i) - 97);
+
+		i++;
+	}
+
+	return (str);
+}
