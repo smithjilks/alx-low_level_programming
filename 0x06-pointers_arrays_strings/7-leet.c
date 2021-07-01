1e37dd1e97acace2e8f7a08cd58ce2a5f8637492
smithjilks <smithjilks@gmail.com>
2021-07-01 20:53:10 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ char *leet(char *str)
 
 	while (*(str + i) != '\0')
 	{
-		if (*(str + i) == 97 || *(str + i) == 122)
+		if (*(str + i) == 65 || *(str + i) == 97)
 			*(str + i) = 52;
 
 		if (*(str + i) == 69 || *(str + i) == 101)
