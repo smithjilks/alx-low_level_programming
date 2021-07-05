0417b5307568c4aaed28654cf0b11a3900d54c31
smithjilks <smithjilks@gmail.com>
2021-07-05 21:05:37 +0300

    Add 3-strspn.c file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -25,6 +25,7 @@ char *_strchr(char *s, char c)
 		{
 			return (s + i);
 		}
+
 		i++;
 	}
 
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/*file: 3-strspn.c*/
+/**
+ * _strspn - returns the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ *
+ * @s : string parameter
+ * @accept : bytes to compare in s
+ *
+ * Return : number of bytes from initial segment
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, n;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		n = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				n = 0;
+				break;
+			}
+		}
+
+		if (n == 1)
+			break;
+	}
+
+	return (i);
+}
