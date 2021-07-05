4bdb421672975bb01843aeb67cceb84c477c78d4
smithjilks <smithjilks@gmail.com>
2021-07-05 20:31:59 +0300

    Add 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/* file: 2-strchr.c*/
+/**
+ * _strchr - returns a pointer to the first accurrence
+ * of the character c in the string
+ *
+ * @s : string in which to find the first occurrence of char c
+ * @c : char to find in string s
+ *
+ * Return: pointer to the fist occurence of char c, NULL if not found
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
