aaace4745a816242444278469ac874abf536c9af
smithjilks <smithjilks@gmail.com>
2021-07-05 23:06:40 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,13 +15,16 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
 	}
+
+	if (*(s + i) == c)
+		return (s + i);
 
 	return ('\0');
 }
