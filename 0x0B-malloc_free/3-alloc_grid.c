caefcf5730e8d30c97c2428521795e1c2b06d493
smithjilks <smithjilks@gmail.com>
2021-07-13 22:45:58 +0300

    Add 3-alloc_grid.c file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+/*file: 3-alloc_grid.c*/
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ *
+ * @width: string to be concatenated
+ * @height: string to be concatenated
+ *
+ * Return: returns a pointer to a 2 dimensional array of integers.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		if (grid[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+
+	return (grid);
+
+}
