18c5828ff5e47078d33dea9f2204b1a377a0e14b
smithjilks <smithjilks@gmail.com>
2021-08-04 22:07:18 +0300

    Rename 101-print_list_safe.c to 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,6 +20,21 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointer of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
+
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
@@ -35,5 +50,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
