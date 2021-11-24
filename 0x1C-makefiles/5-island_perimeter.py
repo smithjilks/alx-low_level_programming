7efe4ebaed8573c1e5c5818b6170d8c5c6c83f5a
smithjilks <smithjilks@gmail.com>
2021-11-24 23:52:26 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,13 +18,13 @@ def island_perimeter(grid):
 def get_surrounding_water(grid, i, j):
     water_sides = 0
 
-    if grid[i][j+1] == 0:
+    if grid[i][j + 1] == 0 or j >= len(grid[i]) - 1:
         water_sides += 1
-    if grid[i-1][j] == 0:
+    if grid[i - 1][j] == 0 or i <= 0:
         water_sides += 1
-    if grid[i][j-1] == 0:
+    if grid[i][j - 1] == 0 or j <= 0:
         water_sides += 1
-    if grid[i+1][j] == 0:
+    if grid[i + 1][j] == 0 or i >= len(grid) - 1:
         water_sides += 1
 
     return water_sides
